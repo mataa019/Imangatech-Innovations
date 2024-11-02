953d11884ac60f0c99c93963178939a5e489e02e
John Mataa <johnnymataa019@gmail.com>
2024-11-02 10:08:22 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,116 @@
+$(document).ready(function() {
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+  }
+
+  function slideElements() {
+    $('.mottor-item').each(function() {
+      if (isElementInViewport(this)) {
+        $(this).addClass('slide-in');
+      }
+    });
+  }
+
+  slideElements(); // Call the function to trigger the initial sliding animation
+
+  $(window).scroll(function() {
+    slideElements();
+  });
+});
+
+$(document).ready(function () {
+  //Owl
+  $(".hero-slider").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    dots: false,
+    navText: ["PREV", "NEXT"],
+    smartSpeed: 1000,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    responsive: {
+      0: {
+        nav: false,
+      },
+      768: {
+        nav: true,
+      },
+    },
+  });
+
+  $("#projects-slider").owlCarousel({
+    loop: true,
+    nav: false,
+    items: 2,
+    dots: true,
+    smartSpeed: 600,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+        margin: 8,
+      },
+    },
+  });
+
+  $(".reviews-slider").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    smartSpeed: 900,
+    items: 1,
+    margin: 24,
+    autoplay: true,
+    autoplayTimeout: 4000,
+  });
+});
+
+// back top
+var btn = $("#back-to-top");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
+  // Price carousel
+  $(".price-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 45,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        992:{
+            items:2
+        },
+        1200:{
+            items:3
+        }
+    }
+});
